eea6843ff7a2beacf14f43c2b6a149e5b06aa266
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-06-14 03:06:38 -0300

    feat: add Dao interface for basic CRUD

---
+++ src/main/java/org/clients/DaoCrud.java
@@ -0,0 +1,17 @@
+package org.clients;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DaoCrud<T> {
+    void save(T t);
+
+    void saveAll(List<T> lt);
+
+    void update(T t);
+    void delete(T t);
+
+    Optional<T> getById(String idColumnName, String id);
+
+    List<T> getAll();
+}
